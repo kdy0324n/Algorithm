89c6180e3d4c37baacc83c69cea6bb8bf5316046
kdy0324n <kdy0324n@naver.com>
2022-03-11 17:04:37 +0900

    20020311

---
+++ baekjoon/1620.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+map<string,string>Map;
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,m;
+    cin>>n>>m;
+    for(int i=1;i<=n;i++){
+        string str;cin>>str;
+        string str_i = to_string(i);
+        Map.insert({str,str_i});
+        Map.insert({str_i,str});
+    }
+    for(int i=1;i<=m;i++){
+        string str;
+        cin>>str;
+        cout<<Map[str]<<'\n';
+    }
+
+    return 0;
+}
---
+++ baekjoon/1764.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n,m;
+set<string>s;
+vector<string>ans;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin>>n>>m;
+
+    for(int i=1;i<=n;i++){
+        string str;cin>>str;
+        s.insert(str);
+    }
+    for(int i=1;i<=m;i++){
+        string str;cin>>str;
+        if(!s.insert(str).second){
+            ans.push_back(str);
+        }
+    }
+    sort(ans.begin(),ans.end());
+    cout<<ans.size()<<'\n';
+    for(auto i:ans){
+        cout<<i<<'\n';
+    }
+
+    return 0;
+}
